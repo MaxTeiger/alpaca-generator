078f3214742d3b968039a93570e440c8dc9d8d38
Max <max.teiger@gmail.com>
2021-12-11 23:00:15 +0100

    feat: begin logic implementation

--- scripts/main.js
+++ scripts/main.js
@@ -1,59 +1,52 @@
-let bckImgPath = "img/backgrounds/";
-let backgroundImages = {
-  "Blue 50": "blue50.png",
-  "Blue 60": "blue60.png",
-  "Blue 70": "blue70.png",
-  "Dark Blue 30": "darkblue30.png",
-  "Dark Blue 50": "darkblue50.png",
-  "Dark Blue 70": "darkblue70.png",
+"use strict";
 
-  "Green 50": "green50.png",
-  "Green 60": "green60.png",
-  "Green 70": "green70.png",
+// // Avatar
+// const neck = document.querySelector(".neck");
+// const mouth = document.querySelector(".mouth");
+// const hair = document.querySelector(".hair");
+// const leg = document.querySelector(".leg");
+// const ears = document.querySelector(".ears");
+// const eyes = document.querySelector(".eyes");
 
-  "Grey 40": "grey40.png",
-  "Grey 70": "grey70.png",
-  "Grey 80": "grey80.png",
+// // Buttons
+// const hairB = document.querySelector(".btn_hair");
+// const earsB = document.querySelector(".btn_ears");
+// const eyesB = document.querySelector(".btn_eyes");
+// const mouthB = document.querySelector(".btn_mouth");
+// const neckB = document.querySelector(".btn_neck");
+// const legB = document.querySelector(".btn_leg");
+// const accessoriesB = document.querySelector(".btn_accessories");
+// const backgroundB = document.querySelector(".btn_background");
 
-  "Red 50": "red50.png",
-  "Red 60": "red60.png",
-  "Red 70": "red70.png",
+// function which triggers when a primary button is clicked
+// and populate secondary-buttons with appropriate ones
+let primaryBtns = document.querySelectorAll(".primary_button");
+primaryBtns.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    if (localStorage.getItem("currentButton")) {
+      document
+        .getElementById(localStorage.getItem("currentButton"))
+        .classList.toggle("active");
+    }
 
-  "Yellow 50": "yellow50.png",
-  "Yellow 60": "yellow60.png",
-  "Yellow 70": "yellow70.png",
-};
+    if (localStorage.getItem("currentMenu")) {
+      document
+        .getElementById(localStorage.getItem("currentMenu"))
+        .classList.toggle("collapsed");
+    }
 
-var fs = require("fs");
-var files = fs.readdirSync("img/backgrounds");
-console.log(files);
+    localStorage.setItem("currentButton", e.target.id);
+    localStorage.setItem("currentMenu", "style-" + e.target.id);
 
-// Attributes Buttons
-let backgroundButton = document.querySelector("#btn_background");
-let earsButton = document.querySelector("#btn_ears");
-let accessoriesButton = document.querySelector("#btn_accessories");
-let eyesButton = document.querySelector("#btn_eyes");
-let hairButton = document.querySelector("#btn_hair");
-let legButton = document.querySelector("#btn_leg");
-let mouthButton = document.querySelector("#btn_mouth");
-let neckButton = document.querySelector("#btn_neck");
+    document
+      .getElementById(localStorage.getItem("currentButton"))
+      .classList.toggle("active");
+    document
+      .getElementById(localStorage.getItem("currentMenu"))
+      .classList.toggle("collapsed");
+  });
+});
 
-// Images layers
-let bckImage = document.querySelector("#Layer1");
-let neckImage = document.querySelector("#Layer2");
-let noseImage = document.querySelector("#Layer3");
-let mouthImage = document.querySelector("#Layer4");
-let legImage = document.querySelector("#Layer5");
-let eyesImage = document.querySelector("#Layer6");
-let earsImage = document.querySelector("#Layer7");
-let hairImage = document.querySelector("#Layer8");
-let accessoriesImage = document.querySelector("#Layer9");
-
-backgroundButton.onclick = function () {
-  let mySrc = bckImage.getAttribute("src");
-  if (mySrc === "img/backgrounds/blue50.png") {
-    bckImage.setAttribute("src", "img/backgrounds/blue50.png");
-  } else {
-    bckImage.setAttribute("src", "img/backgrounds/blue50.png");
-  }
-};
+// fonction which populates secondary buttons regarding which
+// primary button has been clicked
+function displaySecondaryButtons(attribute) {}
